cd4adde029db77c9324b99ea5f52b3ed4da96350
George H. Seelinger <ghseeli@gmail.com>
2025-11-17 14:12:16 -0500

    Initial commit.

---
+++ LeanClub/Pfaffian.lean
@@ -0,0 +1,21 @@
+import Mathlib
+
+/-!
+# Pfaffian
+
+The Pfaffian of an alternating matrix
+
+## Main definitions
+
+## Main results
+
+## TODO
+
+* Define alternating matrices
+* Define perfect matchings
+* Define crossing number of perfect matching
+* Define Pfaffian
+
+## References
+
+-/
